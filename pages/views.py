e8031af67bc48e6ceaaa0d063a1116287850f8e1
Banji <khanjani1997@gmail.com>
2020-12-15 21:00:28 +0330

    Made 2 new function ----> home, about and defining them in pages.urls

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    return render(request, "pages/home.html")
+def about(request):
+    return render(request, "pages/about.html")
