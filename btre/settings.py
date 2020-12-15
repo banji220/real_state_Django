d44ea2bfe27409b17c7a9bc7c18e16e79ba19493
Banji <khanjani1997@gmail.com>
2020-12-15 15:16:42 +0330

    Defined pages.app.PagesConfig in our setting.py

--- btre/settings.py
+++ btre/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.app.PagesConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
