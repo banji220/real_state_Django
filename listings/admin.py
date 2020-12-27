2f882ac9bde41a3b045673cf1b1d684ed13cdc07
Banji <khanjani1997@gmail.com>
2020-12-27 19:30:50 +0330

    Customizing Listing Admin

--- listings/admin.py
+++ listings/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Listing
 
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "is_published")
+
 admin.site.register(Listing)
