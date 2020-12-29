cc3c554a254e8fdae576391801c73dfcb05be19e
Banji <khanjani1997@gmail.com>
2020-12-29 23:25:27 +0330

    Fixed a bug

--- realtors/admin.py
+++ realtors/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import Realtor
 
-class RealtoAdmin(admin.ModelAdmin):
+class RealtorAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "email", "hire_date")
     list_display_links = ("id", "name")
     list_filter = ("name",)
     list_per_page = 25
 
-admin.site.register(Realtor)
+admin.site.register(Realtor, RealtorAdmin)
