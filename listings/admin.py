455e9ee1d5f45fb50fa1bf2f561269386ba6977b
Banji <khanjani1997@gmail.com>
2020-12-29 21:51:40 +0330

    added list_per_page

--- listings/admin.py
+++ listings/admin.py
@@ -4,7 +4,9 @@ from .models import Listing
 class AdminListing(admin.ModelAdmin):
     list_display = ("id", "title", "is_published", "price", "list_date", "realtor")
     list_display_links = ("id", "title")
-    list_filter = ("realtor", "price")
-    
+    list_filter = ("realtor",)
+    list_editable = ("is_published",)
+    search_fields = ("title", "description", "address", "city", "state", "zipcode", "price")
+    list_per_page = 5
     
 admin.site.register(Listing, AdminListing)
