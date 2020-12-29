0a793b86aa2d7f78ad2c08ff10cf55eeb62eb9cc
Banji <khanjani1997@gmail.com>
2020-12-29 10:41:47 +0330

    Created db's again

--- listings/admin.py
+++ listings/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from .models import Listing
 
-class ListingAdmin(admin.ModelAdmin):
-    list_display = ("title", "is_published", "price", "list_date",)
 
-admin.site.register(Listing, ListingAdmin)
+admin.site.register(Listing)
