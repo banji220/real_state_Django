eac9593da522aa068846c684fa24efc2964bebbe
Banji <khanjani1997@gmail.com>
2020-12-29 21:56:54 +0330

    Creating RealtorAdmin class

--- listings/admin.py
+++ listings/admin.py
@@ -7,6 +7,6 @@ class AdminListing(admin.ModelAdmin):
     list_filter = ("realtor",)
     list_editable = ("is_published",)
     search_fields = ("title", "description", "address", "city", "state", "zipcode", "price")
-    list_per_page = 5
+    list_per_page = 25
     
 admin.site.register(Listing, AdminListing)
