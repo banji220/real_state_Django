7b43137a2a4e73e45671a6d6cc93efc3f0696a54
Banji <khanjani1997@gmail.com>
2020-12-31 21:26:19 +0330

    Imported Paginator and set variables for paginatios through lists

--- listings/views.py
+++ listings/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Listing
 
 
 def listings(request):
     listings = Listing.objects.all()
+    paginator = Paginator(listings, 3)
+    page_number = request.GET.get("page")
+    page_listing = paginator.get_page("page_number")
     context = {
-        "listings": listings
+        "listings": page_listing
     }
     
     
     return render(request, "listings/listings.html", context)
 
-def listing(request):
+def listing(request, listing_id):
     return render(request, "listings/listing.html")
 
 def search(request):
