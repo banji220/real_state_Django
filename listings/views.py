d7671420f1502f22c8523748a2ad36a48ab6816c
Banji <khanjani1997@gmail.com>
2021-01-05 23:03:43 +0330

    Add logic to our listings function: 1. # If page is not an integer deliver the first page 2.         # If page is out of range deliver last page of results

--- listings/views.py
+++ listings/views.py
@@ -5,9 +5,17 @@ from .models import Listing
 
 def listings(request):
     page_listings = Listing.objects.order_by("-list_date").filter(is_published=True)
-    paginator = Paginator(page_listings, 6)
+    paginator = Paginator(page_listings, 2)
     page_number = request.GET.get("page")
-    listings = paginator.get_page(page_number)
+    try:
+        listings = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        listings = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        listings = paginator.page(paginator.num_pages)
+        
     context = {
         "listings": listings
     }
