df48f72290f60e8a9157f11d8968d6e2e02007e1
Banji <khanjani1997@gmail.com>
2021-01-05 14:59:51 +0330

    Imported listings function to our home template

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
+from  listings.views import listings
 
 def home(request):
-    return render(request, "pages/home.html")
+    return render(request, "pages/home.html", listings)
 def about(request):
     return render(request, "pages/about.html")
