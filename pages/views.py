30bc21ad32bac57e7962cdba36c8db4d0ce30de7
Banji <khanjani1997@gmail.com>
2021-01-06 14:23:47 +0330

    Imported Listing class to our pages.views

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from listings.models import Listing
-
+from realtors.models import Realtor
 def home(request):
     listings = Listing.objects.order_by("list_date").filter(is_published=True)[:3]
     context = {
